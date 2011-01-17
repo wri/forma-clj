cce8c4d93e7ef535cd8a4ffcb8041fb4846fb8cc
Sam Ritchie <sritchie09@gmail.com>
2011-01-17 18:33:38 -0500

    added gdal dependency to project.

--- project.clj
+++ project.clj
@@ -2,10 +2,12 @@
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.hdfgroup/hdf-java "2.6.1"]
+                 [org.n52.wps.gdal/gdal "1.0.0"]
+                 ;; [org.hdfgroup/hdf-java "2.6.1"]
                  [cascalog "1.6.0-SNAPSHOT"]
                  ]
-  :dev-dependencies [
+    :repositories {"52north" "http://52north.org/maven/repo/releases/"}
+    :dev-dependencies [
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [swank-clojure "1.2.1"]
                      ])
