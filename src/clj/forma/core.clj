7619d63ec5d7fbde6f8ffb40ac242f37ea7e7ba2
Sam Ritchie <sritchie09@gmail.com>
2011-01-26 11:06:42 -0500

    Chunker works for returning TileID. About to get full chunks returned.

--- src/clj/forma/core.clj
+++ src/clj/forma/core.clj
@@ -1,7 +1,7 @@
 (ns forma.core
   (:use cascalog.api)
   (:import [forma WholeFile])
-  (:require [cascalog [ops :as c] [workflow :as w]]
+  (:require [cascalog [vars :as v] [ops :as c] [workflow :as w]]
             [forma [hdf :as h]]))
 
 (defn whole-file
@@ -21,6 +21,40 @@
   (let [source (hfs-wholefile dir)]
     (<- [?file] (source ?file))))
 
+
+;; ##Helper Functions
+
+(defn add-metadata
+  "Takes a seq of keys and an unpacked modis tile, and returns
+   a number of fields based on the size of the input sequence."
+  [modis keys]
+  (let [outargs (v/gen-nullable-vars (count keys))]
+    (<- outargs (modis ?freetile)
+        (h/meta-values [keys] ? :>> outargs))))
+
+;; We currently can't have a subquery that returns any sort
+;; of custom dataset. Once I get a response on the cascalog user group,
+;; we need to go ahead and refactor this.
+(defn unpacked-modis
+  "Returns a stream of 1-tuples containing serialized gdal Datasets."
+  [nasa-dir]
+  (let [nasa-files (all-files nasa-dir)]
+    (<- [?unpacked]
+        (nasa-files ?hdf)
+        (h/unpack ?hdf :> ?unpacked))))
+
+;; ## Example Queries
+;; This is the good stuff!
+
+(defn same-tiles
+  "Refactored version of unique tiles."
+  [nasa-dir]
+  (let [nasa-files (unpacked-modis nasa-dir)
+        meta-vals (add-metadata nasa-files ["TileID"])]
+    (?<- (stdout) [?tileid ?count]
+         (meta-vals ?tileid)
+         (c/count ?count))))
+
 (defn file-count
   "Prints the total count of files in a given directory to stdout."
   [dir]
@@ -29,13 +63,34 @@
      (files ?file)
      (c/count ?count))))
 
+(defn modis-chunks
+  "Chunker."
+  [hdf]
+  (let [keys ["TileID"]]
+    (<- [?tileid]
+        (hdf ?hdf)
+        (h/unpack ?hdf :> ?freetile)
+        (h/meta-values [keys] ?freetile :> ?tileid)
+        (:distinct false))))
+
+;; (defn unique-tiles
+;;   "Processes all HDF files in the supplied directory, and prints the TileIDs
+;;    and their associated counts to standard out."
+;;   [nasa-dir]
+;;   (let [nasa-files (all-files nasa-dir)
+;;         tile-keys ["TileID"]]
+;;     (?<- (stdout) [?tileid ?count]
+;;          (nasa-files ?hdf)
+;;          (h/unpack ?hdf :> ?freetile)
+;;          (h/meta-values [tile-keys] ?freetile :> ?tileid) 
+;;          (c/count ?count))))
+
 (defn unique-tiles
   "Processes all HDF files in the supplied directory, and prints the TileIDs
    and their associated counts to standard out."
   [nasa-dir]
-  (let [nasa-files (all-files nasa-dir)]
+  (let [nasa-files (all-files nasa-dir)
+        chunks (modis-chunks nasa-files)]
     (?<- (stdout) [?tileid ?count]
-         (nasa-files ?hdf)
-         (h/unpack ?hdf :> ?freetile)
-         (h/get-tileid ?freetile :> ?tileid) 
+         (chunks ?tileid)
          (c/count ?count))))
--- src/clj/forma/hdf.clj
+++ src/clj/forma/hdf.clj
@@ -7,16 +7,15 @@
 
 (defn get-metadata
   "Takes in a decompressed HDF file and a key, and returns the associated
-   metadata.
-   [TODO] takes in a seq of metadata, returns a seq of values."
-  [hdf key]
+   metadata."
+  [hdf keys]
   (let [metadata (.GetMetadata_Dict hdf "")]
-    (.get metadata key)))
+    (vec (map #(.get metadata %) keys))))
 
 (defn get-tileid
   "Returns the TileID of a decompressed MODIS file."
   [hdf]
-  (get-metadata hdf "TileID"))
+  (get-metadata hdf ["TileID"]))
 
 (defn get-bytes
   "Extracts a byte array from a Hadoop BytesWritable object."
@@ -24,12 +23,21 @@
   (byte-array (.getLength bytes)
               (.getBytes bytes)))
 
-(defmapop unpack
-  "Stateful approach to unpacking HDF files. Registers all gdal formats,
-   Creates a temp directory, then saves the byte array to disk. This byte
-   array is processed with gdal, and then the temp directory is destroyed.
-   Function returns the decompressed MODIS file as a 1-tuple."
-  {:stateful true}
+
+;; Requires an unpacked MODIS file and a seq of meta-data keys.
+
+(defmapop [meta-values [meta-keys]] [modis]
+  (let [metadata (.GetMetadata_Dict modis "")]
+    (vec (map #(.get metadata %) meta-keys))))
+
+;; ## Mapping operations
+
+;; Stateful approach to unpacking HDF files. Registers all gdal formats,
+;; Creates a temp directory, then saves the byte array to disk. This byte
+;; array is processed with gdal, and then the temp directory is destroyed.
+;; Function returns the decompressed MODIS file as a 1-tuple.
+
+(defmapop unpack {:stateful true}
   ([]
      (do
        (gdal/AllRegister)
@@ -43,15 +51,3 @@
          (gdal/Open (.toString tfile) 0))))
   ([tdir]
      (i/delete-file-recursively tdir)))
-
-(defn unpack-hdf
-  "My first try at unpacking HDF files. This works, but doesn't clean up
-   after decompression. See the defmapop for a better option."
-  [bytestream]
-  (let [hash (Integer/toString (m/abs (.hashCode bytestream)))
-        tfile (i/file (io/temp-dir "hdf") hash)
-        tpath (.toString tfile)]
-    (do
-      (gdal/AllRegister)
-      (i/copy (get-bytes bytestream) tfile)
-      (gdal/Open tpath 0))))
