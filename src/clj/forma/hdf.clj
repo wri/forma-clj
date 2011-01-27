506dd3836570cc9ca89cb5935f2a0b82d056f124
Sam Ritchie <sritchie09@gmail.com>
2011-01-27 18:57:49 -0500

    Added function to pull raster data out of a subdataset and store it in a java int-array.

--- src/clj/forma/hdf.clj
+++ src/clj/forma/hdf.clj
@@ -1,6 +1,7 @@
 (ns forma.hdf
   (:use cascalog.api)
-  (:import [org.gdal.gdal gdal Dataset]
+  (:import [org.gdal.gdal gdal Dataset Band]
+           [org.gdal.gdalconst gdalconstConstants]
            [org.apache.hadoop.io BytesWritable])
   (:require [cascalog [ops :as c] [workflow :as w] [io :as io]]
             [clojure.contrib [math :as m] [io :as i] [string :as s]]))
@@ -8,7 +9,8 @@
 ;; ##Constants
 
 (def
-  #^{:doc "Map between symbols and chosen substrings of MODIS subdataset keys."}
+  #^{:doc "Map between symbols and chosen substrings of
+MODIS subdataset keys."}
   *modis-subsets*
   {:evi "monthly EVI"
    :qual "VI Quality"
@@ -25,6 +27,11 @@
   #^{:doc "Default set of data for FORMA processing."}
   *forma-subsets*
   #{:ndvi :evi :qual :reli})
+
+(def
+  #^{:doc "Arbitrary number of pixels in a chunk of MODIS data."}
+  *chunk-size*
+  24000)
 
 ;; ##Helper Functions
 ;; These are organized fairly well... more docs later.
@@ -44,14 +51,14 @@
 
 (defn subdataset-filter
   "Generates a predicate function that compares Hashtable entries to a supplied
-   list of acceptable keys. These keys should exist in *modis-subsets*."
-  [to-keep]
-  (let [kept-keys (map *modis-subsets* to-keep)]
+   set of acceptable keys. These keys should exist in *modis-subsets*."
+  [good-keys]
+  (let [kept-substrings (map *modis-subsets* good-keys)]
     (fn [entry]
       (let [val (.getValue entry)
             key (.getKey entry)]
         (and (s/substring? "_NAME" key)
-             (some #(s/substring? % val) kept-keys))))))
+             (some #(s/substring? % val) kept-substrings))))))
 
 (def forma-dataset? (subdataset-filter *forma-subsets*))
 
@@ -73,20 +80,33 @@
   (let [path (.toString hdf-file)]
     (metadata (gdal/Open path) "SUBDATASETS")))
 
+(defn raster-array
+  "Unpacks the data inside of a MODIS band into a 1xN integer array."
+  [^Dataset data]
+  (let [band (.GetRasterBand data 1)
+        type (gdalconstConstants/GDT_UInt16)
+        width (.GetXSize band)
+        height (.GetYSize band)
+        ret (int-array (* width height))]
+    (do (.ReadRaster band 0 0 width height type ret)
+        ret)))
+
 ;; ##Cascalog Custom Functions
 ;; These guys actually get called by the queries inside of core.
 
 (defmapop
-  #^{:doc "Generates metadata values for a given unpacked MODIS Dataset and a seq of keys."}
+  #^{:doc "Generates metadata values for a given unpacked MODIS
+Dataset and a seq of keys."}
   [meta-values [meta-keys]] [modis]
   (let [metadata (metadata modis "")]
     (map #(.get metadata %) meta-keys)))
 
 (defmapcatop
-  #^{:doc "Stateful approach to unpacking HDF files. Registers all gdal formats,
-Creates a temp directory, then saves the byte array to disk. This byte
-array is processed with gdal, and then the temp directory is destroyed.
-Function returns the decompressed MODIS file as a 1-tuple."}
+  #^{:doc "Stateful approach to unpacking HDF files. Registers all
+gdal formats, Creates a temp directory, then saves the byte array to
+disk. This byte array is processed with gdal, and then the temp
+directory is destroyed. Function returns the decompressed MODIS file
+as a 1-tuple."}
   unpack {:stateful true}
   ([]
      (do
