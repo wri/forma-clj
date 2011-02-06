f685d9ce687c0942a71b16efd6087f04b975b354
Sam Ritchie <sritchie09@gmail.com>
2011-02-06 02:54:51 -0500

    Added rough timeseries function.

--- src/clj/forma/trends.clj
+++ src/clj/forma/trends.clj
@@ -1,3 +1,13 @@
 ;; This is where we'll be implementing OLS regression.
 
-(ns forma.trends)
+(ns forma.trends
+  (:use cascalog.api))
+
+(defaggregateop timeseries
+  ^{:doc "Takes in a number of vectors, pre-sorted by time period,
+representing the same MODIS chunk within a dataset, and aggregates
+them by building a time series vector of vectors. Entering chunks
+should be sorted in descending order."}
+  ([] [])
+  ([ts val] (conj ts (vec val)))
+  ([ts] ts))
