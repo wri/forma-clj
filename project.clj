61a7f0e7f9465112fa2bdadc5431380df61b39f2
Sam Ritchie <sritchie09@gmail.com>
2011-02-06 19:30:19 -0500

    Updated project.clj to use my new gdal java bindings upload.

--- project.clj
+++ project.clj
@@ -7,14 +7,13 @@
                  [incanter "1.2.3"]
                  [cascalog "1.7.0-SNAPSHOT"]
                  [clj-time "0.3.0-SNAPSHOT"]
+                 [org.clojars.sritchie09/gdal-java "1.8.0"]
                  ]
-  :native-dependencies [[org.clojars.sritchie09/gdal-java-osx-native-deps "1.0.0"]]
   :repositories {"conjars" "http://conjars.org/repo/"}
   :dev-dependencies [
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [swank-clojure "1.2.1"]
                      [lein-eclipse "1.0.0"]
                      [marginalia "0.2.3"]
-                     [native-deps "1.0.5"]
                      [midje "1.0.1"]
                      ])
