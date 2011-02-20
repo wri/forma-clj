cf16c5453db97723165d8d689dbe8eb98ec3e607
Sam Ritchie <sritchie09@gmail.com>
2011-02-20 17:45:05 -0500

    Finished docs for conversion.clj

--- src/clj/forma/conversion.clj
+++ src/clj/forma/conversion.clj
@@ -8,11 +8,11 @@
 
 ;; In developing the time period conversion functions, we noticed that
 ;; as long as the time period remained consistent from dataset to
-;; dataset, the ability to 0-index became irrelevant.  Our choice of
-;; January 1st, 2000 ensures that, regardless of the date on which the
-;; specific MODIS product became active, datasets at the same temporal
-;; resolution will match up. With that in mind, we chose January 1st
-;; 2000.
+;; dataset, the ability to 0-index each dataset became irrelevant.
+;; Our choice of January 1st, 2000 as a common reference date ensures
+;; that, regardless of the date on which the specific MODIS product
+;; became active, datasets at the same temporal resolution will match
+;; up.
 
 (def ref-date (date-time 2000))
 
@@ -22,7 +22,7 @@
 ;; first of each month, while the others mark off blocks of 16 or 8
 ;; julian days from the beginning of the year.
 ;;
-;;(It's important to note that time periods are NOT measured from the
+;; (It's important to note that time periods are NOT measured from the
 ;; activation date of a specific product. The first available NDVI
 ;; 16-day composite, for example, has a beginning date of February
 ;; 18th 2001, or Julian day 49, the 1st day of the 4th period as
@@ -32,17 +32,15 @@
 ;; further validation for our choice of January 1st, 2000 as a
 ;; consistent reference point.
 ;;
-;; Additionally it should be noted that the final dataset is never
+;; Additionally, it should be noted that the final dataset is never
 ;; full; the 23rd 16-day dataset of the year holds data for either 13
 ;; or 14 days, depending on leap year.)
 ;;
-;; TODO COMPLETE FOM HERE ON OUT
-;; TODO -- what's the point of this file? We allow someone to convert
-;; a date into either 16 day, 8 day, or monthly periods, as defined by
-;; MODIS.
-;; TODO -- should we change this so we can pass the function name
-;; directly? The point of this is that we need some way to deal with
-;; 1000m, 16 day data. Make a note in tracker to deal with this.
+;; So! To construct a date, the user supplies a number of date
+;; "pieces", ranging from year down to microsecond. To compare two
+;; times on julian day, we use the clj-time library to calculate the
+;; interval between a given date and January 1st of the year in which
+;; the date occurs.
 
 (defn in-days
   "Returns the number of days spanned by the given time interval."
@@ -58,6 +56,20 @@
   (in-days
    (interval (date-time (year a))
              a)))
+
+;; the julian function complements the other "date-piece"
+;; functions supplied by the clj-time library; day, month, year, and
+;; the others each allow for extraction of that particular element of
+;; a date. We define the delta function to allow us to take the
+;; difference
+;;
+;;    (delta julian a b)
+;;
+;; returns the difference between the julian day values of the two
+;; supplied dates. (Note that `(partial delta julian)` ignores the
+;; year value of each of the dates. That function returns the same
+;; value for a = Jan 1st, 2000, b = Feb 25th, 2000 and b = Feb 25th,
+;; 2002.
 
 (defn delta [f a b] (- (f b) (f a)))
 
@@ -78,9 +90,16 @@
     (+ (* years (per-year unit span))
        (quot units span))))
 
+;; The following partial functions take care of the issue with delta
+;; above, by calculating the span in periods across years.
+
 (def months (partial delta-periods month 1))
 (def sixteens (partial delta-periods julian 16))
 (def eights (partial delta-periods julian 8))
+
+;; We currently choose the period-func based on the resolution of the
+;; data. This will have to be amended, as some datasets at different
+;; temporal resolutions do indeed have the same spatial resolution.
 
 (def period-func
   {"1000" months
