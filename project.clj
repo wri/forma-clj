aa4fddae18f8df040cc695abecd78e7798bd0706
Sam Ritchie <sritchie09@gmail.com>
2011-03-06 18:16:33 -0600

    delete crane stuff. We're going to go with pallet, for this one.

--- project.clj
+++ project.clj
@@ -10,11 +10,14 @@
                  [incanter "1.2.3"]
                  [cascalog "1.7.0-SNAPSHOT"]
                  [clj-time "0.3.0-SNAPSHOT"]
-                 [crane "0.0.2-SNAPSHOT"]
+                 [org.cloudhoist/pallet "0.4.3"]
+                 [org.cloudhoist/pallet-crates-standalone "0.4.0"]
+                 [org.jclouds/jclouds-all "1.0-beta-8"]
                  [org.clojars.sritchie09/gdal-java-native "1.8.0"]]
   :repositories {"conjars" "http://conjars.org/repo/"}
   :dev-dependencies [
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [swank-clojure "1.2.1"]
+                     [cake-pallet "0.4.0"]
                      [marginalia "0.3.2"]
                      [midje "1.0.1"]])
