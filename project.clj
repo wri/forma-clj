b456840c718899998dafabf5262d50f71409063f
Sam Ritchie <sritchie09@gmail.com>
2011-03-18 12:00:42 -1000

    Updated project.clj for use with pallet.

--- project.clj
+++ project.clj
@@ -7,18 +7,36 @@
   :javac-options {:debug "true" :fork "true"}
   :repositories {"conjars" "http://conjars.org/repo/"
                  "sonatype" "http://oss.sonatype.org/content/repositories/releases"}
-  :tasks [cake-pallet.tasks]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [incanter "1.2.3"]
                  [cascalog "1.7.0-SNAPSHOT"]
                  [clj-time "0.3.0-SNAPSHOT"]
-                 [org.cloudhoist/pallet "0.4.3"]
-                 [org.cloudhoist/pallet-crates-standalone "0.4.0"]
-                 [org.jclouds/jclouds-all "1.0-beta-8"]
-                 [org.clojars.sritchie09/gdal-java-native "1.8.0"]]
+                 [org.clojars.sritchie09/gdal-java-native "1.8.0"]
+                 [org.cloudhoist/pallet "0.4.12"
+                  :exclusions [org.jclouds/jclouds-compute
+                               org.jclouds/jclouds-blobstore
+                               org.jclouds/jclouds-scriptbuilder
+                               org.jclouds/jclouds-aws
+                               org.jclouds/jclouds-bluelock
+                               org.jclouds/jclouds-gogrid
+                               org.jclouds/jclouds-rackspace
+                               org.jclouds/jclouds-rimuhosting
+                               org.jclouds/jclouds-slicehost
+                               org.jclouds/jclouds-terremark
+                               org.jclouds/jclouds-jsch
+                               org.jclouds/jclouds-log4j
+                               org.jclouds/jclouds-enterprise]]
+                 [org.jclouds/jclouds-all "1.0-beta-9b"]
+                 [org.jclouds.driver/jclouds-jsch "1.0-beta-9b"]
+                 [org.jclouds.driver/jclouds-log4j "1.0-beta-9b"]
+                 [org.jclouds.driver/jclouds-enterprise "1.0-beta-9b"]
+                 [org.cloudhoist/automated-admin-user "0.4.0"]
+                 [org.cloudhoist/java "0.4.0"]
+                 [com.jcraft/jsch "0.1.42"]
+                 [log4j/log4j "1.2.14"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [swank-clojure "1.2.1"]
-                     [cake-pallet "0.4.0"]
                      [marginalia "0.5.0"]
-                     [midje "1.0.1"]])
+                     [midje "1.0.1"]
+                     [vmfest/vmfest "0.2.2"]])
