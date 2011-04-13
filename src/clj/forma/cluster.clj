864aac1ca664fbaabbd74bd9aabba78c6c283499
Sam Ritchie <sritchie09@gmail.com>
2011-04-13 11:35:04 -0400

    added classes for cluster configuration.

--- src/clj/forma/cluster.clj
+++ src/clj/forma/cluster.clj
@@ -1,5 +1,8 @@
 (ns forma.cluster
   (:require [pallet.core :as c]
-            [pallet.compute :as compute]))
+            [pallet.compute :as compute])
+  (:import [forma FloatsSerialization IntsSerialization]
+           [cascading.tuple.hadoop BytesSerialization TupleSerialization]
+           [org.apache.hadoop.io.serializer WritableSerialization JavaSerialization]))
 
 ;; Nothing here, yet!
