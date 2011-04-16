edf233a8dce7901598b3dcae50e48e8417526abe
danhammer <dan.s.hammer@gmail.com>
2011-04-16 19:21:02 -0400

    added sample rain to ndvi_filter.clj; continued to annotate and specify whizbang function within trends/analysis.clj

--- src/clj/forma/trends/analysis.clj
+++ src/clj/forma/trends/analysis.clj
@@ -1,16 +1,17 @@
 (ns forma.trends.analysis
   (:use [forma.matrix.utils :only (variance-matrix average)]
-        [forma.trends.filter :only (deseasonalize)])
+        [forma.trends.filter :only (deseasonalize)]
+        [forma.presentation.ndvi-filter :only (ndvi reli rain)])
   (:require [incanter.core :as i]))
 
-;; The following is a set of functions, under the header WHOOPBANG,
-;; which collects the greatest short-term drop in a preconditioned and
-;; filtered time-series of a vegetation index.  Currently, the final
-;; function requires three values: (1) time-series as a vector or
-;; sequence (2) a scalar parameter indicating the length of the block
-;; to which the ordinary least squares regression in applied, which
-;; was 15 in the original FORMA specification (3) the length of the
-;; window that smooths the OLS coefficients, which was originally 5.
+;; NOTE Currently, the NDVI and reliability sample time-series comes from the NDVI
+;; trends presentation.  We will have to start a test data file at
+;; some point, but I don't want to fuck with the directory structure
+;; right now, while I am learning to navigate the project.
+
+;; The first few functions are more general, and could be pulled into,
+;; say, the matrix operation namespace.  For now, we will leave them
+;; here, until we can talk more about what goes into that namespace.
 
 (defn time-trend-cofactor
   "construct a matrix of cofactors; first column is comprised of ones,
@@ -25,12 +26,23 @@
   (let [ycol (i/trans [ts])
         X (time-trend-cofactor (count ts))
         V (variance-matrix X)]
-  (i/sel (i/mmult V (i/trans X) ycol) 1 0)))
+    (i/sel (i/mmult V (i/trans X) ycol) 1 0)))
 
 (defn windowed-apply 
   "apply a function [f] to a window along a sequence [xs] of length [window]"
   [f window xs]
   (map f (partition window 1 xs)))
+
+;; WHOOPBANG
+
+;; The following is a routine, under the header WHOOPBANG,
+;; which collects the greatest short-term drop in a preconditioned and
+;; filtered time-series of a vegetation index.  Currently, the final
+;; function requires three values: (1) time-series as a vector or
+;; sequence (2) a scalar parameter indicating the length of the block
+;; to which the ordinary least squares regression in applied, which
+;; was 15 in the original FORMA specification (3) the length of the
+;; window that smooths the OLS coefficients, which was originally 5.
 
 (defn whoopbang
   "whoopbang will find the greatest OLS drop over a timeseries [ts] given 
@@ -42,3 +54,44 @@
        (windowed-apply average window)
        (reduce min)))
 
+;; WHIZBANG
+
+;; These functions will extract the OLS coefficient associated with a
+;; time trend, along with the t-statistic on that coefficient.  For
+;; this, we need a time-series of a vegetation index, and a
+;; corresponding time-series for rain.  This could be made more
+;; general; and in fact, there is a lm (linear model) function in
+;; Incanter that can do this work.  This function, however, collects
+;; all sorts of other information that we do not need, which may slow
+;; processing.  These functions are very tightly tailored to the
+;; function that they serve within the total FORMA process.  We assume
+;; that we only are every concerned with the coefficient on the time
+;; trend (which is reasonable, given the purpose for this routine).  
+
+(defn with-rain-cofactor
+  "construct the cofactors for the Whizbang regression.  This is a
+  special case of a more general regression with many more columns.
+  This assumes that the time-series are already of the same length."
+  [veg-ts rain-ts]
+  {:pre (= (count rain-ts) (count veg-ts))}
+  (i/bind-columns (time-trend-cofactor (count veg-ts)) (i/matrix rain-ts)))
+
+(defn std-error
+  "get the time-trend coefficient's standard error from a variance matrix
+  and the vegetation time-series"
+  [veg-ts cofactor-matrix var-matrix]
+  veg-ts)
+
+(defn ols-coeff
+  "get the trend coefficient from a time-series, given a variance matrix"
+  [veg-ts cofactor-matrix var-matrix]
+  (let [ycol (i/matrix veg-ts)]
+    (i/sel (i/mmult var-matrix (i/trans cofactor-matrix) ycol) 1 0)))
+
+(defn whizbang
+  "extract both the OLS trend coefficient and the t-stat associated
+   with the trend characteristic"
+  [veg-ts rain-ts]
+  (let [cofactor-matrix (with-rain-cofactor veg-ts rain-ts) 
+        var-matrix (variance-matrix cofactor-matrix)]
+    ((juxt ols-coeff std-error) veg-ts cofactor-matrix var-matrix)))
