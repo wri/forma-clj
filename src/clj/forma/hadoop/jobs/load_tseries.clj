c342db3ce6e60ec0f9705540e0c036e29b6a8d2e
Sam Ritchie <sritchie09@gmail.com>
2011-04-21 15:54:22 -0400

    Added predicate macros in load_tseries.

--- src/clj/forma/hadoop/jobs/load_tseries.clj
+++ src/clj/forma/hadoop/jobs/load_tseries.clj
@@ -9,15 +9,32 @@
   (:require [cascalog.ops :as c])
   (:gen-class))
 
+(def
+  ^{:doc "Predicate macro aggregator that extracts a timeseries, given
+  `?chunk`, `?temporal-resolution` and `?date` dynamic vars."}
+  extract-tseries
+  (<- [?temporal-res ?date ?chunk :> ?pix-idx ?t-start ?t-end ?tseries]
+      (datetime->period ?temporal-res ?date :> ?tperiod)
+      (:sort ?tperiod)
+      (timeseries ?tperiod ?int-chunk :> ?pix-idx ?t-start ?t-end ?tseries)))
+
+(def
+  ^{:doc "Predicate macro function to extract relevant spatial data
+  from a given set of chunk fields."}
+  extract-attrs
+  (<- [?tilestring ?spatial-res ?chunk-size ?chunkid ?pix-idx :> ?tile-h ?tile-v ?sample ?line]
+      (tilestring->hv ?tilestring :> ?tile-h ?tile-v)
+      (tile-position ?spatial-res ?chunk-size ?chunkid ?pix-idx :> ?sample ?line)))
+
 (defn process-tseries
+  "Given a source of chunks, this subquery generates timeseries with
+  all relevant accompanying information."
   [chunk-source]
   (<- [?dataset ?spatial-res ?temporal-res ?tile-h ?tile-v ?sample ?line ?pix-idx ?t-start ?t-end ?tseries]
       (chunk-source ?dataset ?spatial-res ?temporal-res ?tilestring ?date ?chunkid ?int-chunk)
-      (datetime->period ?temporal-res ?date :> ?tperiod)
-      (tilestring->hv ?tilestring :> ?tile-h ?tile-v)
-      (:sort ?tperiod)
-      (timeseries ?tperiod ?int-chunk :> ?pix-idx ?t-start ?t-end ?tseries)
-      (tile-position ?spatial-res chunk-size ?chunkid ?pix-idx :> ?sample ?line)))
+      (count ?int-chunk :> ?chunk-size)
+      (extract-tseries ?temporal-res ?date ?int-chunk :> ?pix-idx ?t-start ?t-end ?tseries)
+      (extract-attrs ?tilestring ?spatial-res ?chunkid ?pix-idx :> ?tile-h ?tile-v ?sample ?line)))
 
 (defn -main
   "TODO: Docs.
