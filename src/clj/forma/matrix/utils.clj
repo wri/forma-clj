4d0f6f917fcedc326368d99de60ec97162ab376b
danhammer <dan.s.hammer@gmail.com>
2011-04-22 14:52:31 -0400

    finished annotating utils.clj; merging feature branch.

--- src/clj/forma/matrix/utils.clj
+++ src/clj/forma/matrix/utils.clj
@@ -1,37 +1,24 @@
 (ns forma.matrix.utils
   (:require [incanter.core :as i]))
 
-;; Useful general functions 
-
-(defn variance-matrix
-  "construct a variance-covariance matrix from a given matrix X"
-  [X]
-  (let [product (i/mmult (i/trans X) X)]
-    (i/solve (if (seq? product)
-               product
-               (vector product)))))
-
-(defn column-matrix
-  "create an incanter column matrix of the supplied value with length
-  `x`."
-  [val cols]
-  (i/matrix val cols 1))
-
-(def ones-column (partial column-matrix 1))
-
-(defn above-x? [x] (partial < x))
+;; Useful general functions for matrix operations or filters. The
+;; first functions should be very simple functions used as composites
+;; or filters.  The latter functions should be matrix operations that
+;; deal with arbitrarily large, multi-dimensional matrices.
 
 (defn replace-val
-  "replaces values in a vector [vec] that are (<, >, >=, <=, =) to [in-val]
-  with the value supplied in [out-val]"
+  "replaces values in a vector `vec` that are (<, >, >=, <=, =) relative
+  to `in-val` with the value supplied in `out-val`"
   [vec rel in-val out-val]
   (map #(if (rel % in-val) out-val %) vec))
 
 (defn replace-in
- "replace the value in coll at index idx with value val"
+ "replace the value in `coll` at index `idx` with value `val`"
   [coll idx val]
   {:post [(= (count coll) (count %))]}
   (concat (take idx coll) (list val) (drop (inc idx) coll)))
+
+(defn above-x? [x] (partial < x))
 
 (defn average
   "average of a list"
@@ -52,3 +39,24 @@
                 (empty? tup-seq)) (persistent! v)
                 (= idx pos) (recur (inc idx) (rest tup-seq) (conj! v val))
                 :else       (recur (inc idx) tup-seq (conj! v placeholder))))))
+
+;; Multi-dimensional matrix operations
+
+(defn variance-matrix
+  "construct a variance-covariance matrix from a given matrix `X`. If the
+  value of the matrix multiplication yields an integer value, then we
+  vectorize.  This would be equivalent to the var function in incanter.stats."
+  [X]
+  (let [product (i/mmult (i/trans X) X)]
+    (i/solve (if (seq? product)
+               product
+               (vector product)))))
+
+(defn column-matrix
+  "create an incanter column matrix of the supplied value with length
+  `x`."
+  [val cols]
+  (i/matrix val cols 1))
+
+(def ones-column (partial column-matrix 1))
+
