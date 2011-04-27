3d8df2c0a2eff433dd05f6a1dbbd2f9dcfcb45b8
Sam Ritchie <sritchie09@gmail.com>
2011-04-27 13:21:52 -0400

    Added thrift file to dev. Added shell script for generating new java classes. (We can change this into a cake task inside of project.clj, pretty soon.

--- src/clj/forma/source/fire.clj
+++ src/clj/forma/source/fire.clj
@@ -6,7 +6,7 @@
   (:require [forma.hadoop.predicate :as p]
             [cascalog.ops :as c]
             [cascalog.vars :as v])
-  (:import [forma.type FireTuple]))
+  (:import [forma.schema FireTuple]))
 
 (def prepath "/Users/sritchie/Desktop/FORMA/FIRE/")
 (def testfile (str prepath "MCD14DL.2011074.txt"))
@@ -35,28 +35,33 @@
   [val]
   (fn [v] (> v 50)))
 
+(defpredsummer per-day
+  [val]
+  identity)
+
 (defpredsummer both-preds
   [conf temp]
   (fn [c t] (and (> t 330)
                 (> c 50))))
 
 (def fire-characteristics
-  (<- [?conf ?kelvin :> ?temp-330 ?conf-50 ?both-preds ?max-t]
+  (<- [?conf ?kelvin :> ?temp-330 ?conf-50 ?both-preds ?max-t ?count]
       ((c/juxt #'fires-above-330 #'c/max) ?kelvin :> ?temp-330 ?max-t)
       (conf-above-50 ?conf :> ?conf-50)
+      (per-day ?conf :> ?count)
       (both-preds ?conf ?kelvin :> ?both-preds)))
 
 ;; ## Fire Queries
 
 (defn tupleize
-  [max-t t-above-330 c-above-50 both-preds]
-  (FireTuple. max-t t-above-330 c-above-50 both-preds 1))
+  [max-t t-above-330 c-above-50 both-preds count]
+  (FireTuple. max-t t-above-330 c-above-50 both-preds count))
 
 (defn fire-source
   "Takes a source of textlines, and returns 2-tuples with latitude and
   longitude."
   [source t-res]
-  (let [vs (v/gen-non-nullable-vars 4)]
+  (let [vs (v/gen-non-nullable-vars 5)]
     (<- [?dataset ?datestring ?t-res ?lat ?lon ?tuple]
         (source ?line)
         (identity "fire" :> ?dataset)
