d1f82995e88f9bfb2bd20a00a52102f562d135db
Sam Ritchie <sritchie09@gmail.com>
2011-04-28 02:14:25 -0400

    fake break.

--- test/forma/source/static_test.clj
+++ test/forma/source/static_test.clj
@@ -6,6 +6,8 @@
 ;; Create a sample cascalog source comprised of MODIS resolution,
 ;; horizontal tile, vertical tile, row (line) and column (sample).
 
+(fact (+ 1 1) => 3)
+
 (def temp-mod-source
   (memory-source-tap [["1000" 28 7 208 2]
                       ["1000" 28 7 208 3]
