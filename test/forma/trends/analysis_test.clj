5073c4c58cce52e201e94760ffefb7d292d4cc3f
danhammer <dan.s.hammer@gmail.com>
2011-05-04 18:49:19 -0400

    fixed bad reference in analysis-test ns

--- test/forma/trends/analysis_test.clj
+++ test/forma/trends/analysis_test.clj
@@ -16,5 +16,5 @@
 
 (fact
  "check that whoopbang output is the correct shape for the estimation months"
- (count (:for-est (whoopbang ndvi reli-test 71 75 131 15 5)))
+ (count (:for-est (whoopbang ndvi (vec reli) 71 75 131 15 5)))
  => (inc (- 131 75)))
