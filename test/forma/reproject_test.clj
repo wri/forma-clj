e41cbbcab0d39ae9a34935491f938836ba1a294c
Sam Ritchie <sritchie09@gmail.com>
2011-05-10 19:58:18 -0700

    fixed test in reproject.

--- test/forma/reproject_test.clj
+++ test/forma/reproject_test.clj
@@ -1,12 +1,16 @@
 (ns forma.reproject-test
   (:use [forma.reproject] :reload)
-  (:use midje.sweet))
+  (:use midje.sweet
+        [forma.matrix.utils :only (rowcol->idx)]))
 
-(let [rain-indexer (wgs84-indexer "1000" 0.5 + + -90 0)]
-  (fact
-   "This is the only one I have so far... but we need to verify at
+(let [step 0.5
+      [width] (dimensions-for-step step)
+      rain-indexer (wgs84-indexer "1000" step + + -90 0)]
+  (facts
+    "This is the only one I have so far... but we need to verify at
 least one of these indices."
-   (rain-indexer 8 6 12 12) => 172569))
+    (rain-indexer 8 6 12 12) => [239 489]
+    (rowcol->idx width 239 489) => 172569))
 
 (fact (dimensions-for-step 0.5) => [720 360])
 (fact (fit-to-grid 0.5 + + -90 0 -42.1 0) => [95 0])
