1c2a08f2f1ac7baff922cc88ede1c1ed7fd1fc55
Sam Ritchie <sritchie09@gmail.com>
2011-08-15 00:32:50 -0400

    Added stub method for execute.

--- leiningen/cluster.clj
+++ leiningen/cluster.clj
@@ -1,11 +1,6 @@
 (ns leiningen.cluster
   (:use [leiningen.compile :only (eval-in-project)]))
 
-(defn destroy-cluster []
-  `(do (println "Running destroy-cluster.")
-       (forma.hadoop.cluster/destroy-cluster)
-       (println "Cluster destroyed! Hit Control-C to exit.")))
-
 (defn create-cluster
   [node-type node-count]
   `(do (println (str (format "Running create-cluster of type %s, with %d nodes."
@@ -13,15 +8,25 @@
        (forma.hadoop.cluster/create-cluster ~node-type ~node-count)
        (println "Cluster created! Hit Control-C to exit.")))
 
+(defn destroy-cluster []
+  `(do (println "Running destroy-cluster.")
+       (forma.hadoop.cluster/destroy-cluster)
+       (println "Cluster destroyed! Hit Control-C to exit.")))
+
+(defn execute-jar
+  "TODO: Fill this out with code for uberjarring, uploading to
+  jobtracker and running a command as the hadoop user."  [])
+
 (defn cluster
-  "Hadoop cluster management task! TODO: Describe subtasks."
+  "Start or destroy a pallet-hadoop cluster."
   ([project]
      (println
       "Go ahead and supply a subtask. Options are create and destroy."))
   ([project subtask & args]
      (let [task-func (case subtask
                            "create" create-cluster
-                           "destroy" destroy-cluster)]
+                           "destroy" destroy-cluster
+                           "execute" execute-jar)]
        (eval-in-project project
                         (apply task-func args)
                         nil nil
