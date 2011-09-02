05e90e07d66e7ea1f973d507b610772e0aa001ac
Sam Ritchie <sritchie09@gmail.com>
2011-09-02 15:06:09 -0700

    more script updates.

--- dev/redd.sh
+++ dev/redd.sh
@@ -18,11 +18,13 @@ sudo apt-get install -y --force-yes libhdf4-dev
 # FWTOOLS
 wget -S -T 10 -t 5 http://$bucket.s3.amazonaws.com/$fwtools
 sudo mkdir -p /usr/local/fwtools
-sudo tar -C /usr/local/fwtools -xzf $fwtools
+sudo tar -C /usr/local/fwtools --strip-components=2 -xvzf $fwtools
+sudo chown hadoop $fwtools
 
 # Native Bindings
 wget -S -T 10 -t 5 http://$bucket.s3.amazonaws.com/$native
 sudo mkdir -p /home/hadoop/native
-sudo tar -C /home/hadoop/native -xzf $native
+sudo tar -C /home/hadoop/native --strip-components=2 -xvzf $native
+sudo chown hadoop $native
 
 echo "export LD_LIBRARY_PATH=/usr/local/fwtools/usr/lib" >> /home/hadoop/conf/hadoop-user-env.sh
