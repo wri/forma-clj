ad1fdac65fef6b8f189d34ba688f66bda1ee5c54
Sam Ritchie <sritchie09@gmail.com>
2011-09-02 14:48:32 -0700

    fixed emr bootstrap task.

--- dev/redd.sh
+++ dev/redd.sh
@@ -7,11 +7,20 @@ set -e
 bucket=reddconfig
 fwtools=FWTools-linux-x86_64-4.0.0.tar.gz
 native=linuxnative.tar.gz
+sources=/etc/apt/sources.list
 
+# Install HFD4
+sudo chown hadoop $sources
+echo "deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu maverick universe  multiverse" > $sources
+sudo apt-get update
+sudo apt-get install -y --force-yes libhdf4-dev
+
+# FWTOOLS
 wget -S -T 10 -t 5 http://$bucket.s3.amazonaws.com/$fwtools
 sudo mkdir -p /usr/local/fwtools
 sudo tar -C /usr/local/fwtools -xzf $fwtools
 
+# Native Bindings
 wget -S -T 10 -t 5 http://$bucket.s3.amazonaws.com/$native
 sudo mkdir -p /home/hadoop/native
 sudo tar -C /home/hadoop/native -xzf $native
