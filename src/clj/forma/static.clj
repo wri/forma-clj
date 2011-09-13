ef108315d9f91e0de1bdf5a551326f486904f39b
robinkraft <rkraft4@gmail.com>
2011-09-13 14:43:10 -0300

    Updated static documentation for gadm, ecoid, hansen, and vcf.

--- src/clj/forma/static.clj
+++ src/clj/forma/static.clj
@@ -32,27 +32,41 @@
    gadm :: administrative boundaries, found
    [here](http://goo.gl/2N5CT), and converted to a raster with the
    cell value given by ID_2, an integer value corresponding to the
-   most detailed administrative unit.
+   most detailed administrative unit. .01 degree resolution chosen 
+   for simplicity, but should have limited impact on where a pixel
+   falls aside from at borders.
 
    ecoid :: ecoregions according to the World Wildlife Fund. The
    technical paper can be found [here](http://goo.gl/yvssq). The
    polygons are converted to a raster for sampling - much like the
    admin boundaries - where each cell is assigned the value of the
-   ecoregion id, the most detailed eco unit.
+   ecoregion id, the most detailed eco unit. .01 degree resolution
+   chosen for simplicity, but should have limited impact on where 
+   a pixel falls aside from at borders.
 
    hansen :: forest cover loss hotspots data for training at 500m
    resolution, found [here](http://goo.gl/HqvCW). Note that this has
    been projected into WGS84 from Sinusoidal (463.3127m res).  The
-   time period is 2000-2005.
+   time period is 2000-2005. UPDATE: This may be superceded by the 
+   use of an unreprojected ascii grid that lines up perfectly with
+   MODIS data and can therefore be assigned to MODIS pixels with
+   no error from reprojection.
 
    vcf :: vegetation continuous field index, derived from MODIS
    products for the year 2000, found [here](http://goo.gl/KW8y1), and
    used to define the extent of the sample area, given by the Forest
-   Cover Loss Hotspots training data set.
+   Cover Loss Hotspots training data set. UPDATE: This may be 
+   superceded by the use of an unreprojected ascii grid that lines up 
+   perfectly with MODIS data and can therefore be assigned to MODIS 
+   pixels with no error from reprojection.
 
-   border :: distance in meters to a coastline, defined by the border
-   of land and large body of water.  Projected in MODIS coordinates
-   (463m resolution)." }
+   border :: index from 1-20 representing 500m increments of distance
+   to a coastline as defined by the GADM Level 0 dataset available at 
+   GADM.org. Coastlines are defined as the border between land and large
+   bodies of water. Land is defined by the polygon of country boundaries.
+   Projected World Sinusoidal for distance calculations with ~463m 
+   resolution. Then reprojected WGS84 with .0083333 resolution for 
+   conversion to ascii grid, with bilinear interpolation of pixel values."}
   static-datasets
   {:precl  {:corner [0 -90]
             :travel [+ +]
