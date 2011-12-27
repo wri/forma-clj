8a87e2de14e29f65420035f09401ac8557ed8a51
Sam Ritchie <sritchie09@gmail.com>
2011-12-27 12:46:03 -0500

    Removed thrift dependencies.

--- project.clj
+++ project.clj
@@ -15,12 +15,8 @@
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [incanter/incanter-core "1.3.0-SNAPSHOT"]
                  [clj-time "0.3.3"]
-                 [redd/thrift "0.5.0"]
                  [forma/gdal "1.8.0"]
-                 [commons-lang "2.6"]   ;required for thrift
                  [cascalog "1.8.5-SNAPSHOT"]
-                 [backtype/cascading-thrift "0.1.0"
-                  :exclusions [backtype/thriftjava]]
                  [backtype/dfs-datastores "1.0.5"]
                  [backtype/dfs-datastores-cascading "1.0.5"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
