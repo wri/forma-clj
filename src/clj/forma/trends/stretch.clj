49e6dce0e5cef3fbdd676a5d5570f651e59987bd
Robin Kraft <robin@whizbangsystems.net>
2012-01-20 18:18:38 -0800

    Fixed period bug in stretch, added basic tests

--- src/clj/forma/trends/stretch.clj
+++ src/clj/forma/trends/stretch.clj
@@ -3,22 +3,32 @@
   (:require [forma.date-time :as date]
             [forma.schema :as schema]))
 
+
+
+(defn beg-end [base-res target-res start-idx end-idx]
+  (map (partial date/shift-resolution base-res target-res)
+                       [start-idx end-idx]))
+
+(defn get-day-seq
+  [start-idx series pd val base-res offset]
+  (->> (map-indexed #(vector (+ % start-idx) %2) series)
+       (mapcat (fn [[pd val]]
+                 (repeat (date/period-span base-res pd) val)) )
+       (drop offset)))
+
 (defn ts-expander
   "`series` must be a sequence of 2-tuples of the form `[period, val]`."
   [base-res target-res tseries]
   (let [{:keys [start-idx end-idx series]} tseries
-        [beg end] (map (partial date/shift-resolution base-res target-res)
-                       [start-idx end-idx])
+        [beg end] (beg-end base-res target-res start-idx end-idx)
         offset (date/date-offset target-res beg base-res start-idx)]
     (loop [[pd & more :as periods] (range beg (inc end))
-           day-seq (->> (map-indexed #(vector (+ % beg) %2) series)
-                        (mapcat (fn [[pd val]]
-                                  (repeat (date/period-span base-res pd) val)))
-                        (drop offset))
+           day-seq (get-day-seq start-idx series pd val base-res offset)
            result []]
       (if (empty? periods)
         (schema/timeseries-value beg result)
         (let [num-days (date/period-span target-res pd)]
+          (println (count day-seq))
           (recur more
                  (drop num-days day-seq)
                  (conj result (coll-avg (take num-days day-seq)))))))))
--- test/forma/trends/stretch_test.clj
+++ test/forma/trends/stretch_test.clj
@@ -1,3 +1,48 @@
 (ns forma.trends.stretch-test
   (:use forma.trends.stretch
-        midje.sweet))
+        midje.sweet)
+  (:require [forma.date-time :as date]
+            [forma.schema :as schema]
+            [forma.trends.data :as data]
+            [clojure.zip :as zip]))
+
+
+
+
+
+
+
+
+;; [1 2 3]
+;; (1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1)   1/1 - 1/16
+;; 1.0
+;; (1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2)   1/17 - 2/1
+;; 1.0625
+;; (2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2)   2/2 - 2/17
+;; 2.0
+
+
+;; (2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3)   2/18 - 3/5
+;; 2.1875
+
+;; should be
+;; (2 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3)   2/18 - 3/5
+
+
+
+(def ts
+  (schema/timeseries-value 384 [1 2 3]))
+
+(fact
+  (beg-end "32" "16" (:start-idx ts) (:end-idx ts)) => [736 739])
+
+(fact
+  (get-day-seq 384 [1 2 3] 384 1 "32" 0) => [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3])
+
+(fact
+  "Starting with January 2002 (non-leap year), period 384, a 3-period \"32\"-day timeseries."
+  (ts-expander "32" "16" (schema/timeseries-value
+                          384
+                          [1 2 3])) => {:start-idx 736
+                                        :end-idx 739
+                                        :series [1.0 1.0625 2.0 2.3125]})
