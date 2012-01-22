06a09694f3bfe7447863f05291b7db52b29c33b9
Robin Kraft <robin@whizbangsystems.net>
2012-01-21 20:36:15 -0800

    Cleaned up expander, uncovered new bug

--- test/forma/trends/stretch_test.clj
+++ test/forma/trends/stretch_test.clj
@@ -3,46 +3,27 @@
         midje.sweet)
   (:require [forma.date-time :as date]
             [forma.schema :as schema]
-            [forma.trends.data :as data]
-            [clojure.zip :as zip]))
-
-
-
-
-
-
-
-
-;; [1 2 3]
-;; (1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1)   1/1 - 1/16
-;; 1.0
-;; (1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2)   1/17 - 2/1
-;; 1.0625
-;; (2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2)   2/2 - 2/17
-;; 2.0
-
-
-;; (2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3)   2/18 - 3/5
-;; 2.1875
-
-;; should be
-;; (2 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3)   2/18 - 3/5
-
-
-
-(def ts
-  (schema/timeseries-value 384 [1 2 3]))
+            [forma.trends.data :as data]))
 
 (fact
-  (beg-end "32" "16" (:start-idx ts) (:end-idx ts)) => [736 739])
+  (let [ts (schema/timeseries-value 384 [1 2 3])]
+    (shift-periods-target-res "32" "16" (:start-idx ts) (:end-idx ts)) => [736 739]))
 
 (fact
-  (get-day-seq 384 [1 2 3] 384 1 "32" 0) => [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3])
+  (expand-to-days 384 [1 2 3] 384 1 "32" 0) => [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3])
 
 (fact
   "Starting with January 2002 (non-leap year), period 384, a 3-period \"32\"-day timeseries."
-  (ts-expander "32" "16" (schema/timeseries-value
-                          384
-                          [1 2 3])) => {:start-idx 736
-                                        :end-idx 739
-                                        :series [1.0 1.0625 2.0 2.3125]})
+  (let [ts (schema/timeseries-value 384 [1 2 3])
+        output {:start-idx 736 :end-idx 739 :series [1.0 1.0625 2.0 2.3125]}]
+    (ts-expander "32" "16" ts) => output))
+
+(fact
+  "Expand a longer timeseries - in this case one year of NDVI"
+  (let [p32 12 ;; number of periods, 32-day res
+        p16 22 ;; expected number of periods, 16-day res
+        ts (schema/timeseries-value 360 (take p32 data/ndvi))
+        out-ts (ts-expander "32" "16" ts)]
+    (println (date/period->datetime "16" (:start-idx out-ts)))
+    (println (date/period->datetime "16" (:end-idx out-ts)))
+    (count (:series out-ts)) => p16))
