2a2ba639ad7128787c3a7227e8a342c199fd77b5
Dan <dan.s.hammer@gmail.com>
2012-02-01 11:54:57 -0800

    draft cascalog implementation of classifier

--- test/forma/classify/logistic_test.clj
+++ test/forma/classify/logistic_test.clj
@@ -2,7 +2,9 @@
   (:use [forma.classify.logistic] :reload)
   (:use [midje sweet cascalog]
         [clojure-csv.core]
-        [cascalog.api])
+        [cascalog.api]
+        [clojure.test]
+        [midje.cascalog])
   (:import [org.jblas FloatMatrix])
   (:require [incanter.core :as i]
             [forma.testing :as t]))
@@ -79,24 +81,36 @@ first and last specified, as below."
   string, the label, and the feature vector"
   [n]
   (let [ecoid (eco-generator n)
-        obs (map conj ecoid (map vector y) (map vec X)) ]
+        obs (map conj ecoid (map vector y) (map vec X))]
     (vec (take n obs))))
 
-(defbufferop get-probabilities
+(defbufferop logistic-beta-wrap
+  "returns a vector of coefficients that is accepted within the
+  framework of cascalog."
   [tuples]
   (let [y (vec (flatten (map first tuples)))
         X (vec (map second tuples))]
-    (estimated-probabilities y X X)))
+    [[(vec (logistic-beta-vector y X 1e-8 1e-6 250))]]))
 
-(defn show-logistic-results
-  "display the probability and ecoregion for each pixel in std out,
-  based on grouping by ecoregion and `n` observations total"
+(defn show-beta-vector
+  "generate a separate coefficient vector for each ecoregion, and
+  apply the appropriate coefficient vector to each pixel.  Note that
+  `?feat-training` is the feature set over the training period,
+  whereas `?feat-update` is the feature set through the updated
+  interval.  Here, for this example, the two feature sets are
+  identical, which means that the probability output is defined as the
+  probability of deforestation *during* the training period. This
+  seems a little round-about, but this workflow will reduce redundant
+  calculation as we estimate probabilities for more than one time
+  period (or any other time period than the training period)"
   [n]
-  (let [src (create-sample-tap n)]
+  (let [src (create-sample-tap n)
+        beta-gen (<- [?eco ?beta]
+                     (src ?eco ?y ?feat-training)
+                     (logistic-beta-wrap ?y ?feat-training :> ?beta))]
     (?<- (stdout)
          [?eco ?prob]
-         (src ?eco ?y ?X)
-         (get-probabilities ?y ?X :> ?prob))))
-
-
-
+         (src ?eco ?y ?feat-update)
+         (= ?eco "eco1")
+         (beta-gen ?eco ?beta)
+         (logistic-prob ?beta ?feat-update :> ?prob))))
