0034f7757211b2cfc1f3b6d3dc449eeeb706de57
Dan <dan.s.hammer@gmail.com>
2012-03-04 14:11:40 -0800

    cleaned up logistic_test

--- test/forma/classify/logistic_test.clj
+++ test/forma/classify/logistic_test.clj
@@ -10,6 +10,8 @@
             [forma.testing :as t]
             [forma.date-time :as date]))
 
+;; TODO: Write cascalog.midje tests
+
 (defn read-mys-csv
   "returns a properly adjusted list of the malaysia test data."
   [file-name]
@@ -39,69 +41,4 @@
    (last beta)    => (roughly -8.4745)
    (first beta)   => (roughly -1.7796)
    (logistic-prob (initial-beta Xm) x) => 0.5))
-
-
-
-
-;; (def new-prob (estimated-probabilities y X X))
-
-;; (defn thresh-make-binary
-;;   [threshold val]
-;;   (if (< val threshold) 0 1))
-
-;; (def alerts (map (partial thresh-make-binary 0.5) new-prob))
-
-;; (defn eco-generator [n]
-;;   (map vector (map #(if (odd? %) "eco1" "eco2") (range n))))
-
-;; (defn create-sample-tap
-;;   "creates a sample tap with `n` observations, with the ecoregion
-;;   string, the label, and the feature vector"
-;;   [n]
-;;   (let [ecoid (eco-generator n)
-;;         obs (map conj ecoid (map vector (range n)) (map vector y) (map vec X))]
-;;     (vec (take n obs))))
-
-;; (defbufferop [logistic-beta-wrap-test [r c m]]
-;;   "returns a vector of coefficients that is accepted within the
-;;   framework of cascalog."
-;;   [tuples]
-;;   (let [label-seq (flatten (map first tuples))
-;;         feature-mat (map second tuples)]
-;;     [[(logistic-beta-vector label-seq feature-mat r c m)]]))
-
-;; (deftest group-probabilities-test
-;;   "test that the classifier returns two alerts in this example for a
-;;   given ecoregion, constructed \"eco1\".  This method first calculates
-;;   a separate coefficient vector for each ecoregion, and then applies
-;;   the appropriate coefficient vector to each pixel.  Note that
-;;   `?feat-training` is the feature set over the training period,
-;;   whereas `?feat-update` is the feature set through the updated
-;;   interval.  Here, for this example, the two feature sets are
-;;   identical, which means that the probability output is defined as the
-;;   probability of deforestation *during* the training period. This
-;;   seems a little round-about, but this workflow will reduce redundant
-;;   calculation as we estimate probabilities for more than one time
-;;   period (or any other time period than the training period)
-
-;;   FOR THIS EXAMPLE: return the probabilities for the alerts identified
-;;   for the sample ecoregion, eco1, as well as the arbitrary, unique
-;;   pixel identifiers
-
-;;   NOTE!!!  This example perfectly identifies the two alerts in eco1"
-;;   (fact
-;;    (let [src (create-sample-tap 100)
-;;          beta-gen (<- [?eco ?beta]
-;;                       (src ?eco ?pixel-id ?labels ?feat-training)
-;;                       (logistic-beta-wrap-test [1e-8 1e-6 250] ?labels ?feat-training
-;;                                           :> ?beta))
-;;          alerts-query (<- [?eco ?pixel-id ?prob]
-;;                           (src ?eco ?pixel-id ?labels ?feat-update)
-;;                           (= ?eco "eco1")
-;;                           (beta-gen ?eco ?beta)
-;;                           (logistic-prob ?beta ?feat-update :> ?prob)
-;;                           (> ?prob 0.5))]
-;;      alerts-query => (produces [["eco1" [31] 0.9999999999963644]
-;;                                 ["eco1" [49] 0.9999999999765928]]))))
-
 
