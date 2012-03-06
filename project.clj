fe951d3e449d824ef10395d0b613ca56fb100989
Robin Kraft <robin@whizbangsystems.net>
2012-03-05 17:33:48 -0800

    ram to 16gb

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :marginalia {:javascript ["mathjax/MathJax.js"]}
   :javac-options {:debug "true" :fork "true"}
   ;; http://stackoverflow.com/questions/5839359/java-lang-outofmemoryerror-gc-overhead-limit-exceeded
-  :jvm-opts ["-XX:MaxPermSize=128M" "-XX:+UseConcMarkSweepGC" "-Xms1024M" "-Xmx2048M" "-server"]
+  :jvm-opts ["-XX:MaxPermSize=128M" "-XX:+UseConcMarkSweepGC" "-Xms1024M" "-Xmx16384M" "-server"]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.1.0"]
                  [org.clojure/tools.logging "0.2.3"]
