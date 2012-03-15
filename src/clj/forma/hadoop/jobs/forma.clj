6ce60b2697ef03594209be0ed00f97775ae70d12
Robin Kraft <robin@whizbangsystems.net>
2012-03-15 07:34:24 -0700

    Added trap to handle problem tuples

--- src/clj/forma/hadoop/jobs/forma.clj
+++ src/clj/forma/hadoop/jobs/forma.clj
@@ -165,7 +165,8 @@ value, and the aggregate of the neighbors."
         (log/logistic-prob-wrap ?beta ?val ?neighbor-val :> ?prob)
         (log/mk-timeseries ?pd ?prob :> ?prob-series)
         (:distinct false)
-        (= ?pd 827)))
+        (= ?pd 827)
+        (:trap (hfs-seqfile "s3://formaresults/trapped"))))
 
 (defn prep-for-betas
   [dynamic-src static-src]
