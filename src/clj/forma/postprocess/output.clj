33996f0f2e470b30471be35b5523e933e68e7840
robinkraft <rkraft4@gmail.com>
2012-04-29 12:20:23 -0700

    Added usage to mk-date-header

--- src/clj/forma/postprocess/output.clj
+++ src/clj/forma/postprocess/output.clj
@@ -59,6 +59,8 @@ Ex. (backward-looking-mavg 3 [1 2 3 4]) expands to [nil nil 1 2 3 4]. Nils are f
   (reduce str (split date-str #"-")))
 
 (defn mk-date-header
+  "(mk-date-header \"16\" \"p\" (s/timeseries-value 827 (range 141)))
+  => (\"p20051219\" \"p20060101\" \"p20060117\" ... \"p20120117\")"
   [tres prefix ts]
   (map (comp (partial str prefix)
              date-no-sep
