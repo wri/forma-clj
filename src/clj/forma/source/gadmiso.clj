18d4361352c83cdff98c7c5f1c0e36f2168a29fc
Dan Hammer <dan.s.hammer@gmail.com>
2012-07-09 13:04:10 -0700

    fixed gadmiso so that it doesn't use cascalog to read in the admin-map CSV

--- src/clj/forma/source/gadmiso.clj
+++ src/clj/forma/source/gadmiso.clj
@@ -1,23 +1,25 @@
 (ns forma.source.gadmiso
   (:use cascalog.api)
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure-csv.core :as csv]))
 
-(def text-map
-  "Returns a sequence vectorized strings (e.g., [\"AFG,1\"]) from the
-  admin-map located in the resources folder."
-  (first (??- (hfs-textline "resources/admin-map.csv"))))
+(def text-in
+  "Reads `admin-map.csv` into a Clojure data structure, specifically a
+  lazy sequence of vectors of the form [\"AFG\" \"1\"].  Note that the
+  last line is empty, necessitating the use of `butlast`"
+  (butlast (csv/parse-csv (slurp "resources/admin-map.csv"))))
 
 (defn parse-line
-  "Accepts a vectorized string, as outputted by `text-map`, and
-  returns a single hash-map with the GADM ID (integer) as the key and
-  the ISO3 code (string) as the value."
-  [[x]]
-  (let [[iso gadm-str] (string/split x #",")]
-    {(read-string gadm-str) iso}))
+  "Accepts a vector of two strings of the form [\"AFG\" \"1\"],
+  reflecting each element of `text-in`.  Returns a single hash-map
+  with the GADM ID (integer) as the key and the ISO3 code (string) as
+  the value."
+  [[iso gadm-str]]
+  {(read-string gadm-str) iso})
 
 (def gadm-iso
   "Returns a single hash-map that associates all GADMs and ISO codes."
-  (let [small-maps (map parse-line text-map)]
+  (let [small-maps (map parse-line text-in)]
     (apply merge small-maps)))
 
 (defn gadm->iso
@@ -25,4 +27,3 @@
   a string; wraps gadm-iso map for use in a cascalog query"
   [gadm]
   (gadm-iso gadm))
-
