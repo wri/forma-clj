7ed311877a9a69a02a5efc25849746c9b322daf7
Dan Hammer <dan.s.hammer@gmail.com>
2012-07-14 09:07:53 -0700

    fixed tests for CDM namespace; added dev data for tests

--- test/forma/hadoop/jobs/cdm_test.clj
+++ test/forma/hadoop/jobs/cdm_test.clj
@@ -1,21 +1,26 @@
 (ns forma.hadoop.jobs.cdm-test
-  "This namespace defines Midge tests for the forma.hadoop.jobs.cdm namespace."
+  "This namespace defines Midge tests for the forma.hadoop.jobs.cdm
+namespace."
   (:use cascalog.api
         forma.hadoop.jobs.cdm
-        [midje sweet cascalog]))
+        [midje sweet cascalog])
+  (:require [forma.testing :as t]
+            [cascalog.ops :as c]))
 
 (tabular
- (fact
-   "Test hansen->cdm function."
+ (fact "Test hansen->cdm function.  A valid lat-lon should produce the
+specified row; but an invalid lat-lon will not produce a row."
    (hansen->cdm ?src ?zoom ?tres) => (produces ?row))
  ?src ?zoom ?tres ?row 
  [["-87.65005229999997" "41.850033" "1"]] 16 "32" [[16811 24364 16 131]]
- ;; Invalid lat/lon so there should be no result row.
  [["2000" "2000" "1"]] 16 "32" [])
 
-(tabular
- (fact
-   "Test forma->cdm function."  
-   (forma->cdm ?src ?zoom ?tres ?tres-out ?start ?thresh) => (produces ?row))
- ?src ?zoom ?tres ?tres-out ?start ?thresh ?row
- [["500" 28 8 0 0 [1 49 90]]] 16 "16" "32" "2005-12-31" 50 [[51253 30938 16 71]])
+(fact "Check that `forma->cdm` produces the expected output, ready for
+input into CartoDB table."
+  (let [src (hfs-seqfile (t/dev-path "/testdata/output"))
+        gadm-src (hfs-seqfile (t/dev-path "/testdata/gadm-path"))
+        out-tap (forma->cdm src gadm-src 17 "16" "32" "2005-12-31" 50)]
+    (c/first-n out-tap 3)
+    => (produces [[102620 65277 17 71 "IDN" 0.7104166666666597 101.85574611194706]
+                  [102622 65277 17 71 "IDN" 0.7104166666666597 101.85991309892155]
+                  [102625 65277 17 71 "IDN" 0.7104166666666597 101.86824707287063]])))
