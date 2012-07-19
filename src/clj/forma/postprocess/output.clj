3f0572f0879efeef967ec4f22704376d6052cde3
Dan Hammer <dan.s.hammer@gmail.com>
2012-07-18 17:51:38 -0700

    added docs; removed a lot of extraneous functions and associated tests
    fixed bug, induced by removing not-nil? function
    
    fixed bug in post conditions and added tests for small examples
    
    added docs, changed fact to facts

--- src/clj/forma/postprocess/output.clj
+++ src/clj/forma/postprocess/output.clj
@@ -32,17 +32,23 @@
     (backward-looking-mavg 3 [1 2 3 4]) => '(1.0 1.5 2.0 3.0)"
   [window series]
   (let [expanded-ts (concat (repeat (dec window) nil) (flatten series))]
-    (map (comp float average (partial filter not-nil?))
+    (map (comp float average (partial filter #(not (nil? %))))
                   (partition window 1 expanded-ts))))
 
 (defn clean-probs
-  "Accepts a timeseries of probabilities, and returns a 'cleaned' time
-  series.  Specifically, the cleaned timeseries Smooth the
-  probabilities with a backward-looking moving average, make the
+  "Accepts a timeseries of probabilities, and returns a nested vector
+  'cleaned' time series.  Specifically, the cleaned timeseries Smooth
+  the probabilities with a backward-looking moving average, make the
   series monotonically increasing, and finally make each probability
-  0->1 an integer 0->100"
+  0->1 an integer 0->100.  Note that the moving average window, here,
+  is set to 3 periods.  This is hard-coded, since this function's
+  expressed purpose is to clean the FORMA probability time series.
+  That is, in some sense, FORMA is defined by the 3-length window.
+
+  Example usage:
+    (clean-probs [0.1 0.2 0.3 0.4 0.5]) => [[10 15 20 30 40]]"
   [ts]
-  (let [window 3]
-    [(vec (->> (backward-looking-mavg window ts)
-               (reductions max)
-               (map #(round (* % 100)))))]))
+  {:post [(= (count (flatten %)) (count ts))]}
+  [(vec (->> (backward-looking-mavg 3 ts)
+             (reductions max)
+             (map #(round (* % 100)))))])
