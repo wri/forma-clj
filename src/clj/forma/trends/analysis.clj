54e7470741cf23b4588efb9050172078255798d8
Dan Hammer <dan.s.hammer@gmail.com>
2012-08-07 13:15:01 -0700

    removed exception from long-trends

--- src/clj/forma/trends/analysis.clj
+++ src/clj/forma/trends/analysis.clj
@@ -99,16 +99,18 @@
   [y x & {:keys [intercept] :or {intercept true}}]
   (let [_x (if intercept (i/bind-columns (replicate (i/nrow x) 1) x) x)
         xt (i/trans _x)
-        xtx (i/mmult xt _x)
-        coefs (i/mmult (i/solve xtx) xt y)
-        fitted (i/mmult _x coefs)
-        resid (i/to-list (i/minus y fitted))
-        sse (i/sum-of-squares resid)
-        mse (/ sse (- (i/nrow y) (i/ncol _x)))
-        coef-var (i/mult mse (i/solve xtx))
-        std-errors (i/sqrt (i/diag coef-var))
-        t-tests (i/div coefs std-errors)]
-    [coefs t-tests]))
+        xtx (i/mmult xt _x)]
+    (if-not (singular? xtx)
+      (let [coefs (i/mmult (i/solve xtx) xt y)
+            fitted (i/mmult _x coefs)
+            resid (i/to-list (i/minus y fitted))
+            sse (i/sum-of-squares resid)
+            mse (/ sse (- (i/nrow y) (i/ncol _x)))
+            coef-var (i/mult mse (i/solve xtx))
+            std-errors (i/sqrt (i/diag coef-var))
+            t-tests (i/div coefs std-errors)]
+        [coefs t-tests])
+      [nil nil])))
 
 (defn long-stats
   "returns a tuple with the trend coefficient and associated
@@ -124,14 +126,8 @@
   (let [time-step (utils/idx ts)
         X (if (empty? cofactors)
             (i/matrix time-step)
-            (apply i/bind-columns time-step cofactors))
-        result (try (map second (trend-characteristics ts X))
-                    (catch Throwable e
-                      (error (str "TIMESERIES ISSUES: " ts ", " cofactors) e)))]
-    (if (or (nil? result)
-            (string? result))
-      [nil nil]
-      (vec result))))
+            (apply i/bind-columns time-step cofactors))]
+    (map second (trend-characteristics ts X))))
 
 ;; Short-term trend characteristic; supporting functions
 
