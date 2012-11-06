e1c7f6d8408a54da490f4e27f3221786dfec74ac
Robin Kraft <rkraft4@gmail.com>
2012-11-06 13:09:06 -0800

    Screen out invalid fires

--- src/clj/forma/source/fire.clj
+++ src/clj/forma/source/fire.clj
@@ -179,6 +179,21 @@
       (daily-datestring ?datestring :> ?date)
       (fire-pred ?s-lat ?s-lon ?s-kelvin ?s-conf :> ?dataset ?t-res ?lat ?lon ?tuple)))
 
+(defn valid-fire?
+  "Check whether a fire observation is valid. A valid observation starts
+   with a float value, and has `field-count` fields.
+
+   A handful of observations don't meet this standard. Header rows do
+   not, nor do a handful of observations that have extra fields (14 or
+   19, instead of the expected 12)."
+  [line field-count]
+  (let [fire-fields (seq (.split line ","))]
+      (if (and
+           (number? (read-string (first fire-fields)))
+           (= field-count (count fire-fields)))
+        true
+        false)))
+
 (defn fire-source
   "Returns a Cascalog query that creates tuples for new fire format.
 
@@ -195,12 +210,13 @@
     ?lon - The fire longitude as a float.
     ?tuple - The FireTuple Thrift object representing the fire."
   [src]
-  (<- [?dataset ?date ?t-res ?lat ?lon ?tuple]
-      (src ?line)
-      (p/mangle [#","] ?line
-                :> ?s-lat ?s-lon ?s-kelvin _ _ ?date _ _ ?s-conf _ _ _)
-      (= ?s-lat "latitude" :> false)
-      (fire-pred ?s-lat ?s-lon ?s-kelvin ?s-conf :> ?dataset ?t-res ?lat ?lon ?tuple)))
+  (let [expected-fields 12]
+    (<- [?dataset ?date ?t-res ?lat ?lon ?tuple]
+        (src ?line)
+        (valid-fire? ?line expected-fields)
+        (p/mangle [#","] ?line
+                  :> ?s-lat ?s-lon ?s-kelvin _ _ ?date _ _ ?s-conf _ _ _)
+        (fire-pred ?s-lat ?s-lon ?s-kelvin ?s-conf :> ?dataset ?t-res ?lat ?lon ?tuple))))
 
 (defn reproject-fires
   "Returns a Cascalog query that creates DataChunk Thrift objects for fires."
--- test/clj/forma/source/fire_test.clj
+++ test/clj/forma/source/fire_test.clj
@@ -34,6 +34,16 @@
              fire-source-monthly
              (reproject-fires "1000")))
 
+(facts
+  "Test `valid-fire?`"
+  (let [expected-length 12]
+    (valid-fire?
+     (str "latitude,longitude,brightness,scan,track,acq_date"
+          ",acq_time,satellite,confidence,version,bright_t31,frp"))
+    => false
+    (valid-fire? "-16.701,137.752,338.2,1.7,1.3,2012-11-04, 01:25,T,89,5.0       ,298.1,63" expected-length) => true
+    (valid-fire? "0.585,100.415,331.3,1.2,1.1,2012-09-18, 06:40,A,84,5.0A,75,5.0       ,302.4,26.3" expected-length) => false))
+
 (future-fact
  "Test `fire-source`. Source mimics formatting of an input file.
 
