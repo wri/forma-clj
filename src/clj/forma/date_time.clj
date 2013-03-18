f0075e91b30c1358de29dc1715524d79ec2db610
Robin Kraft <rkraft4@gmail.com>
2013-03-18 15:18:35 -0700

    Pulled in Dan's tweaks to date-time namespace for merging ts

--- src/clj/forma/date_time.clj
+++ src/clj/forma/date_time.clj
@@ -332,3 +332,49 @@ in which `string` lies (according to the supplied resolution, `res`)."
           (if out-of-bounds-idx
             (nth v out-of-bounds-idx))
           nil))))
+
+(defn sorted-ts
+  "Accepts a map with date keys and time series values, and returns a
+  vector with the values appropriately sorted.
+
+  Example:
+    (sorted-ts {:2005-12-31 3 :2006-08-21 1}) => (3 1)"
+  [m]
+  [(vals (into (sorted-map) m))])
+
+(defn key->period
+  [t-res k]
+  (datetime->period t-res (name k)))
+
+(defn period->key
+  [t-res pd]
+  (keyword (period->datetime t-res pd)))
+
+(defn key-span
+  "Returns a list of date keys, beginning and end date inclusive.  The
+  first and last keys represent the periods that start just before the
+  supplied boundary dates.
+
+  Example:
+    (key-span :2005-12-31 :2006-01-18 \"16\")
+    => (:2005-12-19 :2006-01-01 :2006-01-17)"
+  [init-key end-key t-res]
+  (let [init-idx (key->period t-res init-key)
+        end-idx  (inc (key->period t-res end-key))]
+    (map (partial period->key t-res)
+         (range init-idx end-idx))))
+
+(defn same-len? [coll1 coll2]
+  (= (count coll1) (count coll2)))
+
+(defn all-unique?
+  [coll]
+  (same-len? coll (set coll)))
+
+(defn vec->ordered-map
+  "Accepts a collection and a key that indicates the first element in
+  the collection, and a temporal resolution."
+  [init-key t-res coll]
+  (let [init (key->period t-res init-key)
+        end-key  (period->key t-res (+ init (count coll)))]
+    (zipmap (key-span init-key end-key t-res) coll)))
