8864e9812904ecbc6929ff3f49091c1a9aade8fd
Robin Kraft <rkraft4@gmail.com>
2013-03-26 21:28:31 -0700

    Queries and helper functions for export to API

---
+++ src/clj/forma/hadoop/jobs/api.clj
@@ -0,0 +1,59 @@
+(ns forma.hadoop.jobs.api
+  "We’ll generate an API master dataset after each update. This
+  dataset will be vertically partitioned into sequence files using a
+  template tap as follows:
+
+  /api/{iso}/{year}"
+  (:use cascalog.api
+        [forma.source.gadmiso :only (gadm->iso)])
+  (:require [cascalog.ops :as c]
+            [forma.postprocess.output :as o]
+            [forma.reproject :as r]
+            [forma.date-time :as date]
+            [forma.utils :as u]
+            [forma.hadoop.predicate :as p]))
+
+(defmapcatop wide->long
+  "Convert a series from wide to long form.
+
+   Usage:
+     (let [start-idx 827
+           src [[1 [1 2 3]]]]
+       (??<- [?id ?pd ?val]
+             (src ?id ?series)
+             (wide->long start-idx ?series :> ?pd ?val)))
+     ;=> [[1 827 1] [1 828 2] [1 829 3]]"
+  [start-idx series]
+  (let [len (count series)
+        idxs (range start-idx (inc (+ start-idx len)))]
+    (map vector idxs series)))
+
+(defn prob-series->tsv-api
+  "Convert output of `forma-estimate` to long form according to API spec.
+   ?year field is intended for use with template tap."
+  [est-map src static-src]
+  (let [nodata (:nodata est-map)
+        est-start (:est-start est-map)
+        t-res (:t-res est-map)]
+    (<- [?lat ?lon ?iso ?gadm ?date ?year ?prob]
+        (src ?s-res ?mod-h ?mod-v ?sample ?line ?start-idx ?prob-series)
+        (static-src ?s-res ?mod-h ?mod-v ?sample ?line _ ?gadm _ _ _)
+        (gadm->iso ?gadm :> ?iso)
+        (o/clean-probs ?prob-series nodata :> ?clean-series)
+        (wide->long ?start-idx ?clean-series :> ?period ?prob)
+        (r/modis->latlon ?s-res ?mod-h ?mod-v ?sample ?line :> ?lat ?lon)
+        (date/period->datetime t-res ?period :> ?date)
+        (date/convert ?date :year-month-day :year :> ?year))))
+
+(defn mk-tsv
+  "Wrapper `for prob-series->tsv-api` handles export via template tap."
+  [est-map forma-path static-path out-path]
+  (let [out-fields ["?lat" "?lon" "?gadm" "?date" "?prob"]
+        template-fields ["?year" "?iso"]
+        src (hfs-seqfile forma-path)
+        static-src (hfs-seqfile static-path)
+        sink (hfs-textline out-path :sinkmode :replace
+                           :sink-template "%s/%s"
+                           :templatefields template-fields
+                           :outfields out-fields)]
+    (?- sink (prob-series->csv-api est-map src static-src))))
---
+++ test/clj/forma/hadoop/jobs/api_test.clj
@@ -0,0 +1,51 @@
+(ns forma.hadoop.jobs.api-test
+  "We’ll generate an API master dataset after each update. This
+  dataset will be vertically partitioned into sequence files using a
+  template tap as follows:
+
+  /api/{iso}/{year}"
+  (:use cascalog.api
+        [midje sweet cascalog])
+  (:use forma.hadoop.jobs.api :reload)
+  (:require [cascalog.ops :as c]
+            [forma.reproject :as r]
+            [forma.date-time :as date]
+            [forma.utils :as u]))
+
+(def est-map {:nodata -9999.0 :est-start "2005-12-19" :t-res "16"})
+
+(def sample-forma-data
+  [["500" 28 8 0 0 827 [0.5 0.49 0.55 0.54 0.60 0.59]]])
+
+(def sample-static-data
+  [["500" 28 8 0 0 57 15319 23 100 20]])
+
+(fact "Test `wide->long`"
+  (let [start-idx 827
+        src [[1 [1 2 3]]]]
+    (<- [?id ?pd ?val]
+        (src ?id ?series)
+        (wide->long start-idx ?series :> ?pd ?val)))
+  => (produces [[1 827 1] [1 828 2] [1 829 3]]))
+
+(fact "Check `prob-series->tsv-api"
+  (prob-series->tsv-api est-map sample-forma-data sample-static-data)
+  => (produces-some
+      [[9.99791666666666 101.54412568476158 "IDN" 15319 "2005-12-19" "2005" 50]
+       [9.99791666666666 101.54412568476158 "IDN" 15319 "2006-01-01" "2006" 50]
+       [9.99791666666666 101.54412568476158 "IDN" 15319 "2006-01-17" "2006" 51]
+       [9.99791666666666 101.54412568476158 "IDN" 15319 "2006-02-02" "2006" 53]
+       [9.99791666666666 101.54412568476158 "IDN" 15319 "2006-02-18" "2006" 56]
+       [9.99791666666666 101.54412568476158 "IDN" 15319 "2006-03-06" "2006" 58]]))
+
+(fact "Check `mk-tsv`"
+  (let [forma-path "/tmp/forma"
+        static-path "/tmp/static"
+        out-path "/tmp/out"]
+    (?- (hfs-seqfile forma-path :sinkmode :replace) sample-forma-data) 
+    (?- (hfs-seqfile static-path :sinkmode :replace) sample-static-data)
+    (mk-tsv est-map forma-path static-path out-path)
+    (hfs-textline (str out-path "/2006/IDN"))
+    => (produces-some [["9.99791666666666\t101.54412568476158\t15319\t2006-01-01\t50"]])))
+
+
