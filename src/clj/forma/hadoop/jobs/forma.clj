ffa4423ea933de67ab192a6dc080155e21252853
Robin Kraft <rkraft4@gmail.com>
2013-03-29 14:25:55 -0700

    Simple function tests performance of regular and optimized trends

--- src/clj/forma/hadoop/jobs/forma.clj
+++ src/clj/forma/hadoop/jobs/forma.clj
@@ -146,6 +146,48 @@
               (a/long-stats ts short-rain)      
               (a/hansen-stat ts)])))
 
+(defn new-trends
+  "Calculates all trend statistics for a timeseries, also returns the
+   period index of the last element of the series"
+  [window long-block start-idx ts rain-ts]
+  (let [end-idx (series-end ts start-idx)
+        short-rain (first (f/shorten-ts ts rain-ts))]
+    (flatten [end-idx
+    ;;          (a/short-stat long-block window ts)
+              (a/long-stats ts short-rain)      
+              (a/hansen-stat ts)])))
+
+(defn test-trends-perf
+  "Rough performance stats:
+
+   hansen-stat - 40 ms
+   long-stats - 55 ms
+   short-stat - 95 ms (in current trends)
+   short-stat - 7 ms (optimized - run once)
+
+Usage:
+     (ts-trends {:est-start \"2005-12-19\" :est-end \"2013-03-06\" :t-res \"16\" :window 10 :long-block 30} 693)"
+  [{:keys [est-start est-end t-res window long-block]}
+   ts-start-period]
+  (let [val-ts (repeatedly 350 (partial rand-int 10000))
+        rain-ts (repeatedly 350 (partial rand-int 10000))
+        [start-idx end-idx] (date/relative-period t-res ts-start-period
+                                          [est-start est-end])
+        tele-start-idx (inc start-idx)
+        tele-end-idx (inc end-idx)
+        calculate #(calculate-trends window long-block
+                                     ts-start-period % rain-ts)
+        new-calculate #(new-trends window long-block
+                                   ts-start-period % rain-ts)]
+    (prn "Normal trends")
+    (time (dotimes [_ 10] (last (first (map (comp vec calculate) ;; force realization
+                                      (f/tele-ts tele-start-idx tele-end-idx val-ts))))))
+    (prn "Optimized trends")
+    (time (dotimes [_ 10]
+            (let [a (a/short-stat long-block window val-ts)
+                  b (last (first (map (comp vec new-calculate) ;; force realization
+                         (f/tele-ts tele-start-idx tele-end-idx val-ts))))])))))
+
 (defmapcatop telescoping-trends
   "Maps `calculate-trends` onto each part of an ever-lengthening subset
    of the input timeseries, from `est-start` to `est-end`. Returns
