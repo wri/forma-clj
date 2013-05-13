4d3010998f325004f4274c9b1c98b8d5ee791514
Robin Kraft <rkraft4@gmail.com>
2013-05-13 16:59:24 -0700

    Support thresholding api output

--- test/clj/forma/hadoop/jobs/api_test.clj
+++ test/clj/forma/hadoop/jobs/api_test.clj
@@ -15,7 +15,8 @@
 (def est-map {:nodata -9999.0 :est-start "2005-12-19" :t-res "16"})
 
 (def sample-forma-data
-  [["500" 28 8 0 0 827 [0.5 0.49 0.55 0.54 0.60 0.59]]])
+  [["500" 28 8 0 0 827 [0.5 0.49 0.55 0.54 0.60 0.59] 88500]
+   ["500" 28 8 0 1 827 [0. 0. 0. 0. 0. 0. 0.] 88500]])
 
 (def sample-static-data
   [["500" 28 8 0 0 88114]])
@@ -28,22 +29,26 @@
         (wide->long start-idx ?series :> ?pd ?val)))
   => (produces [[1 827 1] [1 828 2] [1 829 3]]))
 
-(fact "Check `prob-series->tsv-api`. Note that output probabilities are smoothed."
-  (prob-series->tsv-api est-map sample-forma-data sample-static-data)
+(fact "Check `prob-series->tsv-api`. Note that output probabilities
+       are smoothed. The second test includes a threshold."
+  (prob-series->tsv-api est-map sample-forma-data)
   => (produces-some
-      [[9.99791666666666 101.54412568476158 "IDN" "IDN" 88114 "2005-12-19" "2005" 50]
-       [9.99791666666666 101.54412568476158 "IDN" "IDN" 88114 "2006-01-01" "2006" 50]
-       [9.99791666666666 101.54412568476158 "IDN" "IDN" 88114 "2006-01-17" "2006" 51]
-       [9.99791666666666 101.54412568476158 "IDN" "IDN" 88114 "2006-02-02" "2006" 53]
-       [9.99791666666666 101.54412568476158 "IDN" "IDN" 88114 "2006-02-18" "2006" 56]
-       [9.99791666666666 101.54412568476158 "IDN" "IDN" 88114 "2006-03-06" "2006" 58]]))
+      [[9.99791666666666 101.54412568476158 "IDN" "IDN" 88500 "2005-12-19" "2005" 50]
+       [9.99374999999999 101.542824160711 "IDN" "IDN" 88500 "2006-02-02" "2006" 0]])
+  (let [thresh 50]
+    (prob-series->tsv-api est-map sample-forma-data thresh)
+    => (produces [[9.99791666666666 101.54412568476158 "IDN" "IDN" 88500 "2005-12-19" "2005" 50]
+                  [9.99791666666666 101.54412568476158 "IDN" "IDN" 88500 "2006-01-01" "2006" 50]
+                  [9.99791666666666 101.54412568476158 "IDN" "IDN" 88500 "2006-01-17" "2006" 51]
+                  [9.99791666666666 101.54412568476158 "IDN" "IDN" 88500 "2006-02-02" "2006" 53]
+                  [9.99791666666666 101.54412568476158 "IDN" "IDN" 88500 "2006-02-18" "2006" 56]
+                  [9.99791666666666 101.54412568476158 "IDN" "IDN" 88500 "2006-03-06" "2006" 58]])))
+
 
 (fact "Check `mk-tsv`"
   (let [forma-path "/tmp/forma"
-        static-path "/tmp/static"
         out-path "/tmp/out"]
     (?- (hfs-seqfile forma-path :sinkmode :replace) sample-forma-data) 
-    (?- (hfs-seqfile static-path :sinkmode :replace) sample-static-data)
-    (mk-tsv est-map forma-path static-path out-path)
-    (hfs-textline (str out-path "/2006/IDN"))
-    => (produces-some [["9.99791666666666\t101.54412568476158\tIDN\t88114\t2006-01-01\t50"]])))
+    (mk-tsv est-map forma-path out-path)
+    (hfs-textline (str out-path "/IDN/2006"))
+    => (produces-some [["9.99791666666666\t101.54412568476158\tIDN\t88500\t2006-01-01\t50"]])))
