ab45440aaece0888200f08a19eac3f9ed6e76c06
Robin Kraft <rkraft4@gmail.com>
2013-09-13 13:14:45 -0400

    hotfix: added "gc" command to bootstrap for cloning forma-clj repo

--- dev/forma_bootstrap.sh
+++ dev/forma_bootstrap.sh
@@ -77,7 +77,10 @@ echo "source ~/.git-completion.bash" >> .bashrc
 # simple leiningen install via 'li'
 echo "alias li='cd /home/hadoop/bin; wget https://raw.github.com/technomancy/leiningen/preview/bin/lein; chmod u+x lein; ./lein; cd /home/hadoop;'" >> /home/hadoop/.bashrc
 
+# clone forma-clj repo
+echo "alias gc='git clone git://github.com/reddmetrics/forma-clj.git'" >> /home/hadoop/.bashrc
+
 # alias to do standard forma-clj setup
-setup="li; git clone git://github.com/reddmetrics/forma-clj.git; cd forma-clj; uj; cd ~/;"
+setup="li; gc; cd forma-clj; uj; cd ~/;"
 
 exit 0
