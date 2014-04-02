57d43bbc40bc84a26133e1fe46edd6ff0dd73540
Robin Kraft <rkraft4@gmail.com>
2014-04-01 21:13:50 -0700

    Helper functions generate full table for GFW - no need for sql

--- src/clj/forma/gfw/cdm.clj
+++ src/clj/forma/gfw/cdm.clj
@@ -8,7 +8,9 @@
 
   http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/globalmaptiles.py
   "
-  (:use [clojure.math.numeric-tower :only (expt, round)]))
+  (:use [cascalog.api])
+  (require [clojure.math.numeric-tower :as math]
+           [cascalog.ops :as c]))
 
 ;; Exact radius of the Earth in meters for the WGS84 datum.
 (def radius 6378137)
@@ -46,7 +48,7 @@
     (let [[yt xt] (pixels->tile this)
           zoom (:zoom this)
           ;; Moves coordinate origin from bottom-left to top-left corner of tile
-          yt-flipped (-> (- (expt 2 zoom) 1)
+          yt-flipped (-> (- (math/expt 2 zoom) 1)
                           (- yt))]
       [xt yt-flipped zoom]))
     
@@ -94,7 +96,7 @@
      [this]
      "Returns this alert map resolution in meters per pixel."
      (let [zoom (:zoom this)]
-       (/ initial-res (expt 2 zoom)))))
+       (/ initial-res (math/expt 2 zoom)))))
 
 ;; Map of lat/lon min and max values.
 (def latlon-range {:lat-min -90 :lat-max 90 :lon-min -180 :lon-max 180})
@@ -120,7 +122,7 @@
   [xm ym zoom]
   "Directly converts x and y coordinates to pixel coordinates at zoom level,
    without passing through latlon."
-  (let [res (/ initial-res (expt 2 zoom))
+  (let [res (/ initial-res (math/expt 2 zoom))
         yp (-> (+ ym origin-shift)
                (/ res))
         xp (-> (+ xm origin-shift)
@@ -131,7 +133,7 @@
   [xm ym zoom]
   "Returns the map tile coordinates [x y zoom] for a given x, y (in meters),
    resolution (in meters) and zoom level."
-  (let [res (/ initial-res (expt 2 zoom))
+  (let [res (/ initial-res (math/expt 2 zoom))
         [xp yp] (meters->pixels-direct xm ym zoom)
         yt (-> (/ yp map-tile-dim)
                (Math/ceil)
@@ -141,6 +143,99 @@
                (Math/ceil)
                (- 1)
                (int))
-        yt-flipped (-> (- (expt 2 zoom) 1)
+        yt-flipped (-> (- (math/expt 2 zoom) 1)
                        (- yt))]
     [xt yt-flipped zoom]))
+
+(defbufferop agg-sort-by-date
+  "Aggregate dates and counts by xyz coordinate, sorting the counts by
+  date. See usage example in tests."
+  [tuples]
+  (let [sorted (sort-by first tuples)]
+    [[[(vec (map first sorted)) (vec (map second sorted))]]]))
+
+(defn split-vecs
+  "Split vector of nested vectors into two vectors of vectors. For use with
+   output of `agg-sort-by-date` defbufferop.
+
+   See usage example in tests."
+  [v]
+  [(vec (flatten (first v))) (vec (flatten (second v)))])
+
+(defn vec->arr-str
+  "Format vector as string array format for PostgreSQL."
+  [v]
+  (-> (str v)
+      (clojure.string/replace "[" "{")
+      (clojure.string/replace "]" "}")
+      (clojure.string/replace " " ",")))
+
+(defn arr-str->vec
+  "Convert PostgreSQL array string to vector. Reverses `array-ify`."
+  [s]
+  (-> s
+      (clojure.string/replace "{" "[")
+      (clojure.string/replace "}" "]")
+      (clojure.string/replace "," " ")
+      (read-string)))
+
+(defn agg-xyz
+  []
+  (<- [?x ?y ?z ?period ?count :> ?x2int ?y2int ?z-new ?periods ?counts]
+      (/ ?x 2 :> ?x2)
+      (/ ?y 2 :> ?y2)
+      (math/floor ?x2 :> ?x2int)
+      (math/floor ?y2 :> ?y2int)
+      (dec ?z :> ?z-new)
+      (agg-sort-by-date ?period ?count :> ?periods-counts)
+      (split-vecs ?periods-counts :> ?periods ?counts)))
+
+(defn zoom-out
+  "Calculate x or y value at next zoom level."
+  [n]
+  (math/floor (/ n 2)))
+
+(defn gen-tiles
+  "Generate all tile levels through `min-z` (e.g. 7) for a given
+  initial `xyz` tuple."
+  [x y z min-z]
+  (if (= z min-z)
+     [[x y z]]
+     (conj (gen-tiles (zoom-out x) (zoom-out y) (dec z) min-z) [x y z])))
+
+(defmapcatop gen-tiles-mapcat
+  "Wrap `gen-tiles` in `mapcatop` so each zoom level is emitted on a new line."
+  [x y z min-z]
+  (gen-tiles x y z min-z))
+
+
+(defn gen-all-zooms
+  "Cascalog query generates zoom levels through `min-z` for a given
+  source of `xyz` tuples. For lower zoom levels, sets of `xyz-period`
+  tuples are counted up. For a given `xyz-period` tuple, that gives us
+  the number of pixels at the next higher zoom level that fall within
+  the given `xyz` tuple for a given period."
+  [src min-z]
+  (<- [?x2 ?y2 ?z2 ?period ?count]
+      (src ?x ?y ?z ?period)
+      (gen-tiles-mapcat ?x ?y ?z min-z :> ?x2 ?y2 ?z2)
+      (c/count ?count)))
+
+(defn agg-periods-counts
+  "Cascalog query aggregates periods and counts in long form into vectors.
+
+   For example:
+
+     [[25032 36353 16 71 2]
+      [25032 36353 16 72 1]]
+
+     becomes
+
+     [[25032 36353 16 [71 72] [2 1]]]."
+  [src]
+  (<- [?x ?y ?z ?periods-arr ?counts-arr]
+      (src ?x ?y ?z ?period ?count)
+      (agg-sort-by-date ?period ?count :> ?periods-counts)
+      (split-vecs ?periods-counts :> ?periods ?counts)
+      (vec->arr-str ?periods :> ?periods-arr)
+      (vec->arr-str ?counts :> ?counts-arr)))
